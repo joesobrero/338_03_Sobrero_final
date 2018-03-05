b93969670a645dcb772684c897dfd0df1ff7a918
Joseph Sobrero <joesobrero@gmail.com>
2018-03-05 13:06:45 -0800

    add jquery libraries, add nav content and styles

---
+++ script.js
@@ -0,0 +1,17 @@
+$document.ready(function() {
+    // VARIABLES
+    // colors
+    var black = '#000';
+    var white = '#fff';
+    var gray = '#aaa';
+    var red = '#D81200';
+    var blue = '#022B3A';
+    // event vars
+
+
+    // functions
+
+
+    // events
+
+});
