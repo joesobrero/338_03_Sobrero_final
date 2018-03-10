24a69d2d0e52a21b5d31076f4b6cf6aa4043dc68
Joseph Sobrero <joesobrero@gmail.com>
2018-03-09 18:05:53 -0800

    add new images, add scroll functions to story page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$document.ready(function() {
+$(document).ready(function() {
     // VARIABLES
     // colors
     var black = '#000';
@@ -6,10 +6,50 @@ $document.ready(function() {
     var gray = '#aaa';
     var red = '#D81200';
     var blue = '#022B3A';
+    // selectors
+    var content = $('.container');
+    var lines = $('#top-line,#bot-line');
+    var story = $('#story-scroll');
+    var story1 = $('#container1');
+    var story2 = $('#container2');
+    var scrollTrig = $('#scroll-trig');
+    var storyTop = $('#topofpage');
     // event vars
+    var storyView = 1;
 
 
     // functions
+    function storyScroll() {
+        lines.animate({
+            backgroundColor: red
+        }).delay(800).animate({
+            backgroundColor: black
+        });
+        /*
+        if(storyView == 1) {
+            document.getElementById('container2').scrollIntoView(1000);
+            storyView = 2;
+        }
+        else {
+            document.getElementById('container1').scrollIntoView(1000);
+            storyView = 1;
+        }
+        */
+        if(storyView == 1) {
+            story.animate({
+                scrollTop: story2.offset().top
+            }, 850);
+        }
+        else {
+            story.animate({
+                scrollTop: story1.offset().top
+            }, 850);
+        }
+    }
 
     // events
+    content.fadeIn(800);
+    scrollTrig.scroll(function() {
+        storyScroll();
+    });
 });
