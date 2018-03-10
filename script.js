3d151de81f6de72ada7975b654d25aa2ad4683b2
Joseph Sobrero <joesobrero@gmail.com>
2018-03-10 14:55:21 -0800

    add story scroll buttons

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
     var content = $('.container');
     var lines = $('#top-line,#bot-line');
     var story = $('#story-scroll');
+    var scrollButton = $('#scroll-button');
+    var scrollButton2 = $('#scroll-button-2');
     var story1 = $('#container1');
     var story2 = $('#container2');
     // event vars
@@ -30,10 +32,36 @@ $(document).ready(function() {
             storyView = 1;
         }
     }
+    function storyScrollB() {
+        lines.animate({
+            backgroundColor: red
+        }).delay(800).animate({
+            backgroundColor: black
+        });
+        story.animate({
+            scrollTop: story2.offset().top
+        }, 850);
+    }
+    function storyScrollC() {
+        lines.animate({
+            backgroundColor: red
+        }).delay(800).animate({
+            backgroundColor: black
+        });
+        story.animate({
+            scrollTop: story1.offset().top
+        }, 850);
+    }
 
     // events
     content.fadeIn(800);
     story.scroll(function() {
         storyScroll();
     });
+    scrollButton.click(function() {
+        storyScrollB();
+    });
+    scrollButton2.click(function() {
+        storyScrollC();
+    })
 });
